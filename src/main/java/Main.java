2a52c579c6da1795739731d2f92823eced8d1a87
Fepe7 <andreistaicu568@gmail.com>
2025-06-06 21:44:06 +0200

    Segunda version 2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,6 +28,28 @@ public class Main {
 
 
 
+    public static void cantidad_tipo_incidencias(){
+
+        try {
+
+            Statement sentencia = ConexionBBDD.getConnection().createStatement();
+
+            /*Muestra la cantidad de incidencias abiertas en la base de datos*/
+                    ResultSet resultado = sentencia.executeQuery("SELECT tip.nombre_tipo_incidencia AS tipo, COUNT() AS cantidad\n" +
+                    "FROM incidencia inc, tipo_incidencia tip\n" +
+                    "WHERE inc.id_tipo_incidencia = tip.id_tipo_incidencia\n" +
+                    "GROUP BY tip.nombre_tipo_incidencia;");
+
+            while (resultado.next()) {
+                System.out.println("Hay " + resultado.getInt("cantidad") + " incidencias de " + resultado.getString("tipo"));
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+
 
     public static void main(String[] args) {
         System.out.println("Este codigo es para el proyecto de practicas de programacion");
@@ -36,6 +58,10 @@ public class Main {
 
         /*Muestra la cantidad de incidencias abiertas en la base de datos */
         cantidad_incidencias_abiertas();
+
+        /*Muestra la cantidad de incidencias de cada tipo */
+        cantidad_tipo_incidencias();
+
 
 
 
